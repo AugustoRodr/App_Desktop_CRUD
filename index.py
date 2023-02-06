5317e4c440b74ef25f73f0ecc8a1bd752cd2d355
AugustoRodriguez <110948669+AugustoRodr@users.noreply.github.com>
2023-02-06 20:01:20 -0300

    Update index.py

--- index.py
+++ index.py
@@ -5,47 +5,40 @@ import sqlite3
 class Product:
   db_name='app_db.db'
   def __init__(self,window):
-    #envio la ventana al constructor para luego poder manipularla
     self.wind=window
     self.wind.title('Products Application')
-    #Creo un frame que sea un contenedor. Frame es recuadro que adentro me permite poder tener elementos
-    #parametros: 1_donde va a estar el frame;2_ el titulo del frame
-    frame=LabelFrame(self.wind, text='Registra un nuevo producto')
-    frame.grid(row=0, column=0, columnspan=3, pady=20)# con el metodo grid podemos posicionar nuestro frame
-    # pady-->padding y: es el espaciado interno de cada elemento, esto solo para que no se vean tan juntos
-    
-    #Name input
-    Label(frame, text='Nombre: ').grid(row=1, column=0)# Creo una etiquea
-    self.name=Entry(frame)# Creo un espacio de input o entrada
-    self.name.focus()#Esto hara que cuando apenas se ejecute la aplicacion el cursor estara parpadeando en nombre
-    self.name.grid(row=1,column=1)# Posiciono el espacio de entrada
 
-    #Price input
+    frame=LabelFrame(self.wind, text='Registra un nuevo producto')
+    frame.grid(row=0, column=0, columnspan=3, pady=20)
+
+    #Nombre de entrada
+    Label(frame, text='Nombre: ').grid(row=1, column=0)
+    self.name=Entry(frame)
+    self.name.focus()
+    self.name.grid(row=1,column=1)
+
+    #Precio de entrada
     Label(frame, text='Precio: ').grid(row=2, column=0)
     self.price=Entry(frame)
     self.price.grid(row=2,column=1)
 
-    #Button add Product
-    #parametros Button:1_dentro de donde tiene que estar;2_texto que va a mostrar
+    #Boton de agregar producto
     ttk.Button(frame, text='Guardar Producto',command=self.add_product).grid(row=3,columnspan=2,sticky=W+E)
-    # propiedad 'sticky': sirve para indicar desde donde hasta donde queremos que ocupe nuestro boton;W es oeste y E es este
-    #nota: en command tengo que poner la funcion a ejecutar cuando haga click en el boton pero sin los parentecis
 
-    #mensajes de salida/ esto es para que envie un mensaje cuando haya modificado la base de datos
+    #mensaje
     self.message=Label(text='',fg='red')
     self.message.grid(row=3,column=0,columnspan=2,sticky=W+E)
 
     #Creo una tabla para poder visualizar
     self.tree = ttk.Treeview(height=10,columns=2)
     self.tree.grid(row=4,column=0,columnspan=2)
-    self.tree.heading('#0',text='Nombre',anchor=CENTER)# agrego el nombre de las columnas
+    # agrego el nombre de las columnas
+    self.tree.heading('#0',text='Nombre',anchor=CENTER)
     self.tree.heading('#1',text='Precio',anchor=CENTER)
 
     # otros botones
     ttk.Button(text='ELIMINAR',command=self.delete_product).grid(row=5,column=0,sticky=W+E)
     ttk.Button(text='EDITAR',command=self.edit_product).grid(row=5,column=1,sticky=W+E)
-
-
 
     #llenando la tabla
     self.get_products()
@@ -58,7 +51,7 @@ class Product:
       return resultado
   
   def get_products(self):
-    # lo que hago aqui es limpiar la tabla en caso de que tenga basura y se deja limpio que despues mostrar los nuevos datos que queremos.
+    # limpio la tabla por si tiene basura
     records= self.tree.get_children()
     for elem in records:
       self.tree.delete(elem)
@@ -77,19 +70,17 @@ class Product:
       query='insert into productos values(null,?,?)'
       valores=(self.name.get(),self.price.get())
       self.run_query(query,valores)
-      #en el codigo de message puedo acceder a su artibuto de la siguiente forma para cambiarla
       self.message['text']=f'El Producto {self.name.get()} se guardo satisfactoriamente'
       self.name.delete(0,END)
-      self.price.delete(0,END)# esto lo que hara es vaciar los casilleros de entry una vez que se envie la informacion.
+      self.price.delete(0,END)
     else:
       self.message['text']='El nombre y el precio son requeridos'
     self.get_products()
 
   def delete_product(self):
-    self.message['text']=''# si ya hay un mensaje lo limpio
+    self.message['text']=''
     try:
       self.tree.item(self.tree.selection())['text'][0]
-      #sin ['text'] lo que obtenemos del codigo anterior es un diccionario con diferentes keys y sus values
     except IndexError as e:
       self.message['text']='Porfavor selecciones un registro'
       return
@@ -104,20 +95,16 @@ class Product:
     self.message['text']=''
     try:
       self.tree.item(self.tree.selection())['text'][0]
-      #sin ['text'] lo que obtenemos del codigo anterior es un diccionario con diferentes keys y sus values
     except IndexError as e:
       self.message['text']='Porfavor selecciones un registro'
       return
     old_name= self.tree.item(self.tree.selection())['text']
     old_price= self.tree.item(self.tree.selection())['values'][0]
-    #con Toplevel puedo abrir otra ventana encima de la principal
     self.edit_wind=Toplevel()
     self.edit_wind.title='Editar Producto'
     
     #nombre anterior
     Label(self.edit_wind, text='Nombre anterior: ').grid(row=0,column=1)
-    # en el atributo state colocamos el valor 'readonly' para que el usuario solo pueda ver el valor anterior
-    # en el atributo de textVariable expecificamos que valor se mostrara en el entry
     Entry(self.edit_wind, textvariable= StringVar(self.edit_wind,value=old_name),state='readonly').grid(row=0,column=2)
 
     #nuevo nombre
@@ -133,7 +120,6 @@ class Product:
     Label(self.edit_wind,text='Nuevo Precio: ').grid(row=3,column=1)
     new_price= Entry(self.edit_wind)
     new_price.grid(row=3,column=2)
-    #debo de usar la funcion lambda sino dara un error ya que apenas se lee el codigo se ejecuta la funcion sin abrir la ventana y/o enviar los datos
     Button(self.edit_wind,text='Actualizar', command= lambda: self.edit_records(new_name.get(),old_name,new_price.get(),old_price)).grid(row=4,column=2, sticky=W)
 
 
@@ -146,6 +132,8 @@ class Product:
     self.get_products()
 
 if __name__=='__main__':
-  window=Tk()# devuelve una ventana. Esta ventana es la perteneciente a mi aplicacion
-  aplication=Product(window)# esto es por si devuelve algun dato
-  window.mainloop()# con esto la ventana empezara a ejecutarse
+  window=Tk()
+  aplication=Product(window)
+  window.mainloop()
+  
+  
